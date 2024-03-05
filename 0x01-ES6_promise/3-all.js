fa1b86736a51c3bfb34b556ee7140a47da823bcd
mbuvimbatha <mbuvimbatha@gmail.com>
2024-03-05 17:03:19 +0000

    handling multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils.js'
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]) //Promise.all is suitable because we're handling multiple SUCCESSFUL promises
+  .then(([photo, user]) => {
+  console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  })
+  .catch((error) => {
+  console.log('Signup system offline');
+  });
+}
