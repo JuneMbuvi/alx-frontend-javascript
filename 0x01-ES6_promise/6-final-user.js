6b6be8318bcf4ed3a9508ff9544bde9ac9c323ac
mbuvimbatha <mbuvimbatha@gmail.com>
2024-03-06 14:10:01 +0000

    settle all promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,7 @@
-import signUpUser from './4-user-promise.js'
-import uploadPhoto from './5-photo-reject.js'
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises);
+  return Promise.all(promises);
 }
