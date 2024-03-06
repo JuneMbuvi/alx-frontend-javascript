8f1647bdce0714560ba42b1fe9923958dc171e69
mbuvimbatha <mbuvimbatha@gmail.com>
2024-03-06 12:27:57 +0000

    promise all?

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   .then(([photo, user]) => {
   console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   })
-  .catch((error) => {
+  .catch(error => {
   console.log('Signup system offline');
   });
 }
