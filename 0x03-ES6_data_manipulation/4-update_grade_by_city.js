02c22f679f22d7a6136d8005ca182b70b2015f62
mbuvijune <mbuvijune@gmail.com>
2024-03-12 10:47:29 +0000

    filter then map

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const filteredStudents = students.filter((student) => student.location === city);
   // next, map through the students to get their grades
-  const updatedStudents = filteredStudents.map(student => {
+  const updatedStudents = filteredStudents.map((student) => {
     const grade = newGrades.find((grade) => grade.studentId === student.id);
     if (grade) {
       return { ...student, grade: grade.grade };
