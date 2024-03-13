90c01744dfba759e3b9f12cd0d58ae12c0347b9f
mbuvijune <mbuvijune@gmail.com>
2024-03-13 16:15:29 +0000

    Let's build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,43 @@
+interface Teacher {
+  readonly fisrtName: string;
+  readonly lastName: string;
+  readonly fullTimeEmployee: boolean;
+  readonly yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  readonly numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${teacher[0]}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+class StudentClass implements Student {
+  constructor({ firstName, lastName }: constructorStudentClass) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+interface constructorStudentClass {
+  firstName: string;
+  lastName: string;
+}
+
+interface Student {
+  workOnHomework(): string;
+  displayName(): string;
+}
